aec3723652cc63a46b7e815822f0f85c3229f6bb
Crinax <agreedeth@gmail.com>
2024-12-18 20:21:43 +0300

    feat(404-page): redirect from not found pages to home (#566729)

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -8,6 +8,11 @@ const router = createRouter({
       name: 'home',
       component: () => import('@/pages/home-page.vue'),
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      redirect: '/'
+    }
   ],
 });
 
--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
+import { NFlex, NImage, NText } from 'naive-ui';
 </script>
 
 <template>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
-  <h1>Home</h1>
+  <n-flex align="center" justify="center" vertical size="small" class="app-page__home">
+    <n-image class="app-home__logo" height="300" src="/dlp-meme.png" />
+    <n-text>Welcome to Cyper Shield</n-text>
+    <n-text>Start from settings to setup your client</n-text>
+  </n-flex>
 </template>
+
+<style scoped lange="scss">
+.app-page__home {
+  padding: 32px;
+
+  .app-home__logo {
+    border-radius: 16px;
+  }
+}
+</style>
